a7e7e319d9a9dd0273f760af77ee80f6f67e769d
Philipp Korn <philipp.korn@firstbird.com>
2023-03-28 18:10:23 +0200

    wip

--- main.go
+++ main.go
@@ -1,15 +1,43 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/scyhhe/djafka/internal/djafka"
 )
 
-func main() {
-	// service, err := djafka.NewService()
-	// if err != nil {
-	// 	panic(err)
+func testResetOffsets(service *djafka.Service) {
+	// kafka-consumer-groups --bootstrap-server broker:29092 --group consumer-group-3 --describe
+	// kafka-consumer-groups --bootstrap-server broker:29092 --group consumer-group-3 --reset-offsets --to-earliest --all-topics --execute
+	consumerGroups, err := service.ListConsumerGroups()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("consumer groups:", consumerGroups)
+
+	consumers, err := service.ListConsumers(consumerGroups[0:1])
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("target consumers", consumers)
+
+	partition := consumers[0].TopicPartitions
+
+	fmt.Println("target partition", partition)
+
+	// for _, p := range consumers {
+	// 	partition = append(partition, p.TopicPartitions)
 	// }
-	// defer service.Close()
+	service.ResetConsumerOffsets(consumerGroups[0], 0, partition)
+}
+func main() {
+	config, err := djafka.ReadConfig()
+	service, err := djafka.NewService(config.Connections[0])
+	if err != nil {
+		panic(err)
+	}
+	defer service.Close()
 
 	// topicName, err := service.CreateTopic("tschusch")
 	// if err != nil {
@@ -61,5 +89,7 @@ func main() {
 	// 	messageChan <- "STOP"
 	// }
 
-	djafka.Run()
+	// djafka.Run()
+
+	testResetOffsets(service)
 }

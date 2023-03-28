64253b19c60fb2fea6315c6b44569fad84055036
bruno <bruno.manica@firstbird.com>
2023-03-28 10:07:22 +0200

    styles fuck

--- main.go
+++ main.go
@@ -15,7 +15,8 @@ var baseStyle = lipgloss.NewStyle().
 	BorderForeground(lipgloss.Color("240"))
 
 type model struct {
-	table table.Model
+	connectionTable table.Model
+	selectionTable  table.Model
 }
 
 func (m model) Init() tea.Cmd { return nil }
@@ -26,25 +27,54 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "esc":
-			if m.table.Focused() {
-				m.table.Blur()
+			if m.selectionTable.Focused() {
+				m.selectionTable.Blur()
 			} else {
-				m.table.Focus()
+				m.selectionTable.Focus()
 			}
 		case "q", "ctrl+c":
 			return m, tea.Quit
 		case "enter":
 			return m, tea.Batch(
-				tea.Printf("Let's go to %s!", m.table.SelectedRow()[0]),
+				tea.Printf("Let's go to %s!", m.selectionTable.SelectedRow()[0]),
 			)
 		}
 	}
-	m.table, cmd = m.table.Update(msg)
+	m.selectionTable, cmd = m.selectionTable.Update(msg)
 	return m, cmd
 }
 
 func (m model) View() string {
-	return baseStyle.Render(m.table.View()) + "\n"
+	leftPane := lipgloss.JoinVertical(lipgloss.Left, baseStyle.Render(m.connectionTable.View()), baseStyle.Render(m.selectionTable.View()))
+
+	return leftPane
+}
+
+func buildTable(cols []table.Column, rows []table.Row) table.Model {
+
+	t := table.New(
+		table.WithColumns(cols),
+		table.WithRows(rows),
+		table.WithFocused(true),
+		table.WithHeight(5),
+	)
+
+	s := table.DefaultStyles()
+
+	s.Header = s.Header.
+		BorderStyle(lipgloss.NormalBorder()).
+		BorderForeground(lipgloss.Color("240")).
+		MarginTop(0).
+		BorderBottom(true).
+		Bold(false)
+	s.Selected = s.Selected.
+		Foreground(lipgloss.Color("229")).
+		Background(lipgloss.Color("57")).
+		Bold(false)
+
+	t.SetStyles(s)
+
+	return t
 }
 
 func main() {
@@ -54,43 +84,21 @@ func main() {
 	}
 	defer service.Close()
 
-	topics, err := service.ListTopics()
-	if err != nil {
-		panic(err)
+	topColumns := []table.Column{
+		{Title: "Connection", Width: 30},
 	}
-
-	rows := []table.Row{}
-
-	for _, topic := range topics {
-		rows = append(rows, table.Row{
-			topic,
-		})
-	}
-
-	columns := []table.Column{
+	middleColumns := []table.Column{
 		{Title: "Topic", Width: 30},
 	}
 
-	t := table.New(
-		table.WithColumns(columns),
-		table.WithRows(rows),
-		table.WithFocused(true),
-		table.WithHeight(7),
-	)
+	topRows := []table.Row{{"pretty connection"}, {"lol connection"}, {"wonky connection"}}
+	middleRows := []table.Row{{"Topics"}, {"Consumer Groups"}, {"Info"}}
 
-	s := table.DefaultStyles()
-	s.Header = s.Header.
-		BorderStyle(lipgloss.NormalBorder()).
-		BorderForeground(lipgloss.Color("240")).
-		BorderBottom(true).
-		Bold(false)
-	s.Selected = s.Selected.
-		Foreground(lipgloss.Color("229")).
-		Background(lipgloss.Color("57")).
-		Bold(false)
-	t.SetStyles(s)
+	topTable := buildTable(topColumns, topRows)
+	middleTable := buildTable(middleColumns, middleRows)
 
-	m := model{t}
+	m := model{topTable, middleTable}
+
 	if _, err := tea.NewProgram(m).Run(); err != nil {
 		fmt.Println("Error running program:", err)
 		os.Exit(1)
